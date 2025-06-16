0228fb911a9874d8899d1821916dec5a3775ce8b
Ayan Javed <138676495+AyanJaved@users.noreply.github.com>
2025-06-16 21:24:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -90,6 +90,9 @@ app.use((req, res, next) => {
 //   res.send(regU)
 // })
 // Routes
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
